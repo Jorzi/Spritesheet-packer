fd2b2bdb455aa6cbcbb786e6cfa03c20ce988d1b
Göran Maconi <goranmaconi@gmail.com>
2016-01-25 23:13:06 +0200

    First commit; added project description, created the netbeans project.

---
+++ src/spritesheetpacker/SpritesheetPacker.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spritesheetpacker;
+
+/**
+ *
+ * @author Maconi
+ */
+public class SpritesheetPacker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
